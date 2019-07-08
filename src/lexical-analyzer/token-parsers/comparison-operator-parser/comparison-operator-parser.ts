24f6f4b1b2049bb95ae05433327907d861a336c6
Vova Kulikov <vovakulikov@icloud.com>
2019-07-09 01:28:14 +0300

    Added comparison operator parser

---
+++ src/lexical-analyzer/token-parsers/comparison-operator-parser/comparison-operator-parser.ts
@@ -0,0 +1,40 @@
+import { ITokenParser } from "../parser";
+import { IToken, TokenType } from "../../token";
+
+class ComparisonOperatorParser implements ITokenParser {
+
+  operators: { [key: string]: TokenType} = {
+    '>': TokenType.greaterThan,
+    '>=': TokenType.greatherThanOrEqual,
+    '<': TokenType.lessThan,
+    '<=': TokenType.lessThanOrEqual,
+    '=': TokenType.assign,
+    '==': TokenType.equal,
+  };
+
+  guard(character: string) {
+    return this.operators[character] !== undefined;
+  }
+
+  invoke(startPosition: number, source: string): IToken<string> {
+    let cursor = startPosition;
+    let possibleToken = source[cursor];
+    let tokenType = this.operators[possibleToken];
+
+    while(this.guard(possibleToken)) {
+      cursor++;
+      possibleToken += source[cursor];
+      tokenType = this.operators[possibleToken];
+    }
+
+    return {
+      type: tokenType,
+      value: possibleToken,
+      startPosition: startPosition,
+      endPosition: startPosition + possibleToken.length
+    }
+
+  }
+}
+
+export default ComparisonOperatorParser;
